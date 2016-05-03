541708bf14372e8dfef045143ad78192cb7887cb
Malick <diop-malick@hotmail.fr>
2016-05-03 02:13:56 +0200

    minors corrections

--- src/main/java/net/webapp/jhipsterbank/web/rest/BankAccountResource.java
+++ src/main/java/net/webapp/jhipsterbank/web/rest/BankAccountResource.java
@@ -41,120 +41,107 @@ import com.codahale.metrics.annotation.Timed;
 @RequestMapping("/api")
 public class BankAccountResource {
 
-    private final Logger log = LoggerFactory.getLogger(BankAccountResource.class);
-        
-    @Inject
-    private BankAccountRepository bankAccountRepository;
-    
-    @Inject
-    private BankAccountSearchRepository bankAccountSearchRepository;
-    
-    /**
-     * POST  /bankAccounts -> Create a new bankAccount.
-     */
-    @RequestMapping(value = "/bankAccounts",
-        method = RequestMethod.POST,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<BankAccount> createBankAccount(@RequestBody BankAccount bankAccount) throws URISyntaxException {
-        log.debug("REST request to save BankAccount : {}", bankAccount);
-        if (bankAccount.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("bankAccount", "idexists", "A new bankAccount cannot already have an ID")).body(null);
-        }
-        BankAccount result = bankAccountRepository.save(bankAccount);
-        bankAccountSearchRepository.save(result);
-        return ResponseEntity.created(new URI("/api/bankAccounts/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert("bankAccount", result.getId().toString()))
-            .body(result);
-    }
+	private final Logger log = LoggerFactory.getLogger(BankAccountResource.class);
 
-    /**
-     * PUT  /bankAccounts -> Updates an existing bankAccount.
-     */
-    @RequestMapping(value = "/bankAccounts",
-        method = RequestMethod.PUT,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<BankAccount> updateBankAccount(@RequestBody BankAccount bankAccount) throws URISyntaxException {
-        log.debug("REST request to update BankAccount : {}", bankAccount);
-        if (bankAccount.getId() == null) {
-            return createBankAccount(bankAccount);
-        }
-        BankAccount result = bankAccountRepository.save(bankAccount);
-        bankAccountSearchRepository.save(result);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert("bankAccount", bankAccount.getId().toString()))
-            .body(result);
-    }
+	@Inject
+	private BankAccountRepository bankAccountRepository;
 
-    /**
-     * GET  /bankAccounts -> get all the bankAccounts.
-     */
-    @RequestMapping(value = "/bankAccounts",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-//    public ResponseEntity<List<BankAccount>> getAllBankAccounts(Pageable pageable)
-//        throws URISyntaxException {
-//        log.debug("REST request to get a page of BankAccounts");
-//        Page<BankAccount> page = bankAccountRepository.findAll(pageable); 
-//        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/bankAccounts");
-//        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
-//    }
-    
-	  public ResponseEntity<List<BankAccount>> getAllBankAccounts(Pageable pageable)
-	  throws URISyntaxException {
-	  log.debug("REST request to get a page of BankAccounts");
-	  Page<BankAccount> page = bankAccountRepository.findByUserLogin(SecurityUtils.getCurrentUserLogin(), pageable); 
-	  HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/bankAccounts");
-	  return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+	@Inject
+	private BankAccountSearchRepository bankAccountSearchRepository;
+
+	/**
+	 * POST /bankAccounts -> Create a new bankAccount.
+	 */
+	@RequestMapping(value = "/bankAccounts", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<BankAccount> createBankAccount(@RequestBody BankAccount bankAccount)
+			throws URISyntaxException {
+		log.debug("REST request to save BankAccount : {}", bankAccount);
+		if (bankAccount.getId() != null) {
+			return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("bankAccount", "idexists",
+					"A new bankAccount cannot already have an ID")).body(null);
+		}
+		BankAccount result = bankAccountRepository.save(bankAccount);
+		bankAccountSearchRepository.save(result);
+		return ResponseEntity.created(new URI("/api/bankAccounts/" + result.getId()))
+				.headers(HeaderUtil.createEntityCreationAlert("bankAccount", result.getId().toString())).body(result);
 	}
 
-    
+	/**
+	 * PUT /bankAccounts -> Updates an existing bankAccount.
+	 */
+	@RequestMapping(value = "/bankAccounts", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<BankAccount> updateBankAccount(@RequestBody BankAccount bankAccount)
+			throws URISyntaxException {
+		log.debug("REST request to update BankAccount : {}", bankAccount);
+		if (bankAccount.getId() == null) {
+			return createBankAccount(bankAccount);
+		}
+		BankAccount result = bankAccountRepository.save(bankAccount);
+		bankAccountSearchRepository.save(result);
+		return ResponseEntity.ok()
+				.headers(HeaderUtil.createEntityUpdateAlert("bankAccount", bankAccount.getId().toString()))
+				.body(result);
+	}
 
-    /**
-     * GET  /bankAccounts/:id -> get the "id" bankAccount.
-     */
-    @RequestMapping(value = "/bankAccounts/{id}",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<BankAccount> getBankAccount(@PathVariable Long id) {
-        log.debug("REST request to get BankAccount : {}", id);
-        BankAccount bankAccount = bankAccountRepository.findOne(id);
-        return Optional.ofNullable(bankAccount)
-            .map(result -> new ResponseEntity<>(
-                result,
-                HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+	/**
+	 * GET /bankAccounts -> get all the bankAccounts.
+	 */
+	@RequestMapping(value = "/bankAccounts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<List<BankAccount>> getAllBankAccounts(Pageable pageable) throws URISyntaxException {
+		log.debug("REST request to get a page of BankAccounts");
+		Page<BankAccount> page = bankAccountRepository.findAll(pageable);
+		HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/bankAccounts");
+		return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+	}
 
-    /**
-     * DELETE  /bankAccounts/:id -> delete the "id" bankAccount.
-     */
-    @RequestMapping(value = "/bankAccounts/{id}",
-        method = RequestMethod.DELETE,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public ResponseEntity<Void> deleteBankAccount(@PathVariable Long id) {
-        log.debug("REST request to delete BankAccount : {}", id);
-        bankAccountRepository.delete(id);
-        bankAccountSearchRepository.delete(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("bankAccount", id.toString())).build();
-    }
+	/**
+	 * GET /bankAccounts -> get all the bankAccounts.
+	 */
+	@RequestMapping(value = "/userBankAccounts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<List<BankAccount>> getAllUserBankAccounts(Pageable pageable) throws URISyntaxException {
+		log.debug("REST request to get a page of BankAccounts");
+		Page<BankAccount> page = bankAccountRepository.findByUserLogin(SecurityUtils.getCurrentUserLogin(), pageable);
+		HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/bankAccounts");
+		return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+	}
 
-    /**
-     * SEARCH  /_search/bankAccounts/:query -> search for the bankAccount corresponding
-     * to the query.
-     */
-    @RequestMapping(value = "/_search/bankAccounts/{query:.+}",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE)
-    @Timed
-    public List<BankAccount> searchBankAccounts(@PathVariable String query) {
-        log.debug("REST request to search BankAccounts for query {}", query);
-        return StreamSupport
-            .stream(bankAccountSearchRepository.search(queryStringQuery(query)).spliterator(), false)
-            .collect(Collectors.toList());
-    }
+	/**
+	 * GET /bankAccounts/:id -> get the "id" bankAccount.
+	 */
+	@RequestMapping(value = "/bankAccounts/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<BankAccount> getBankAccount(@PathVariable Long id) {
+		log.debug("REST request to get BankAccount : {}", id);
+		BankAccount bankAccount = bankAccountRepository.findOne(id);
+		return Optional.ofNullable(bankAccount).map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+	/**
+	 * DELETE /bankAccounts/:id -> delete the "id" bankAccount.
+	 */
+	@RequestMapping(value = "/bankAccounts/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Void> deleteBankAccount(@PathVariable Long id) {
+		log.debug("REST request to delete BankAccount : {}", id);
+		bankAccountRepository.delete(id);
+		bankAccountSearchRepository.delete(id);
+		return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("bankAccount", id.toString())).build();
+	}
+
+	/**
+	 * SEARCH /_search/bankAccounts/:query -> search for the bankAccount
+	 * corresponding to the query.
+	 */
+	@RequestMapping(value = "/_search/bankAccounts/{query:.+}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public List<BankAccount> searchBankAccounts(@PathVariable String query) {
+		log.debug("REST request to search BankAccounts for query {}", query);
+		return StreamSupport.stream(bankAccountSearchRepository.search(queryStringQuery(query)).spliterator(), false)
+				.collect(Collectors.toList());
+	}
 }
